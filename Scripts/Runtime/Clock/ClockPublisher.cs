6abb3aac077cfffbd0a198eae43dc79fcdd7fc40
Ryodo Tanaka <groadpg@gmail.com>
2021-09-08 04:37:57 +0900

    Add ROS Clock publishser (#2)
    * Add Rosgraph messages
    
    * Add ROS Clock publisher

---
+++ Scripts/Runtime/Clock/ClockPublisher.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.Rosgraph;
+
+public class ClockPublisher : MonoBehaviour
+{
+
+  [SerializeField] private string _topicName = "clock";
+    
+  private float _timeStamp   = 0f;
+
+  private ROSConnection _ros;
+  private ClockMsg _message;    
+    
+  void Start()
+  {
+    // setup ROS
+    this._ros = ROSConnection.instance;
+    this._ros.RegisterPublisher<ClockMsg>(this._topicName);
+
+    // setup ROS Message
+    this._message = new ClockMsg();
+    this._message.clock.sec = 0;
+    this._message.clock.nanosec = 0;
+  }
+
+    void Update()
+    {
+        this._timeStamp = Time.time;
+        uint sec = (uint)Math.Truncate(this._timeStamp);
+        uint nanosec = (uint)( (this._timeStamp - sec)*1e+9 );
+        this._message.clock.sec = sec;
+        this._message.clock.nanosec = nanosec;
+        this._ros.Send(this._topicName, this._message);
+    }
+}
